6450010e85002d2f5a09d71a5a221d006b22c1e1
Emily E. Wilbourn <wilbourn_ee@cobra.cs.mercer.edu>
2020-11-27 18:15:08 -0500

    initial commit

---
+++ dijkstra.cpp
@@ -0,0 +1,22 @@
+//Emily Wilbourn
+//11-27-2020
+//Program 4 - CSC 245
+#include "graph.h"
+#include <queue>
+#include <string>
+
+struct vertex_info
+{
+	string origin;
+	string destination;
+	
+};
+
+int main()
+{
+	Graph<string> myGraph(50);
+	Queue<string> myQ(50);
+	
+	int numVertices
+	return 0;
+}
