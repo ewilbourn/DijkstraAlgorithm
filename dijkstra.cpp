54794f617af6379e3b6b3dd17724755fdf706da8
Emily E. Wilbourn <wilbourn_ee@cobra.cs.mercer.edu>
2020-11-27 19:01:57 -0500

    reading in the file with the semicolon as a delimeter

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -2,8 +2,10 @@
 //11-27-2020
 //Program 4 - CSC 245
 #include "graph.h"
-#include <queue>
+#include "queue.h"
 #include <string>
+#include <iostream>
+#include <fstream>
 
 struct vertex_info
 {
@@ -13,10 +15,37 @@ struct vertex_info
 	string destination;
 };
 
-int main()
+void initializeArray(vector <vertex_info> &v, string fileName);
+
+int main(int argc, char *argv[])
 {
 	Graph<string> myGraph(50);
 	Queue<string> myQ(50);
-	
+	vector <vertex_info> v;
+
+	string fileName = argv[1];
+
+	initializeArray(v, fileName);
 	return 0;
 }
+
+//read in the data file and fill each slot in array v with 
+//vertex_info structs
+
+//precondition: v is empty
+//postcondition: nothing is returned, but v is filled
+void initializeArray(vector <vertex_info> &v, string fileName)
+{
+	string line;
+	ifstream input(fileName);
+	if (input.is_open())
+	{
+		while (getline (input, line, ';'))
+		{	
+			cout << line << endl;
+		}
+		input.close();
+	}
+}
+
+
